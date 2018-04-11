b9ce4078db487491a7f9c3962fda2d829e7e18db
cplkimth <cplkimth@hotmail.com>
2018-04-11 15:50:42 +0900

    Take 1

---
+++ UnitTestDemo.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDemo.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDemo.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9afe8c2c-7114-4e5b-9ba8-6fb3ae9b07d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestDemo.UnitTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDemo.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void n이_1이면_1이_반환되어야_함()
+        {
+            int result = Program.Fib(1);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void n이_2이면_2가_반환되어야_함()
+        {
+            int result = Program.Fib(2);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void n이_3보다_크면_이전_두_수열의_합을_반환()
+        {
+            int result = Program.Fib(3);
+
+            Assert.AreEqual(Program.Fib(1) + Program.Fib(2), result);
+        }
+
+    }
+}
